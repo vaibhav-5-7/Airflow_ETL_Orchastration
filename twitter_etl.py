6f2ee90a7261e31100ddcddaa394f3606b9ec0f5
Vaibhav <vaibhavpanade57@gmail.com>
2026-05-18 15:31:02 +0530

    Update twitter_etl.py

--- twitter_etl.py
+++ twitter_etl.py
@@ -1,43 +1,51 @@
 import tweepy
-import pandas as pd 
+import pandas as pd
 import json
 from datetime import datetime
-import s3fs 
+import s3fs
 
 def run_twitter_etl():
-
-    access_key = "" 
-    access_secret = "" 
-    consumer_key = ""
-    consumer_secret = ""
-
+    # Twitter API credentials
+    consumer_key = 'YFLFLCN2gGHPD0c3pn0kNCn9O'
+    consumer_secret = 'N83lAHwI4IVoFzRdiJRD4dFMo3FZRA9RYbpZbbhoJ2l6WMaK1R'
+    access_token = '2012575223879528449-0cmePJwOURv2Z2UdOr5M8uxBwCV0ZV'
+    access_token_secret = '8a4D2qNzzrWWHvNKFNlEeaKQ3S4BiTdepvKY6c0c6EQhp'
 
     # Twitter authentication
-    auth = tweepy.OAuthHandler(access_key, access_secret)   
-    auth.set_access_token(consumer_key, consumer_secret) 
+    auth = tweepy.OAuth1UserHandler(
+        consumer_key,
+        consumer_secret
+    )
 
-    # # # Creating an API object 
+    auth.set_access_token(
+        access_token,
+        access_token_secret
+    )
+
     api = tweepy.API(auth)
-    tweets = api.user_timeline(screen_name='@elonmusk', 
-                            # 200 is the maximum allowed count
-                            count=200,
-                            include_rts = False,
-                            # Necessary to keep full_text 
-                            # otherwise only the first 140 words are extracted
-                            tweet_mode = 'extended'
-                            )
 
-    list = []
+    tweets = api.user_timeline(
+        screen_name='elonmusk',
+        include_rts=False,
+        count=200,
+        tweet_mode='extended'
+    )
+
+    tweet_list = []
+
     for tweet in tweets:
+
         text = tweet._json["full_text"]
 
-        refined_tweet = {"user": tweet.user.screen_name,
-                        'text' : text,
-                        'favorite_count' : tweet.favorite_count,
-                        'retweet_count' : tweet.retweet_count,
-                        'created_at' : tweet.created_at}
-        
-        list.append(refined_tweet)
+        refined_tweet = {
+            "user": tweet.user.screen_name,
+            "text": text,
+            "favorite_count": tweet.favorite_count,
+            "retweet_count": tweet.retweet_count,
+            "created_at": tweet.created_at
+        }
 
-    df = pd.DataFrame(list)
-    df.to_csv('refined_tweets.csv')
+        tweet_list.append(refined_tweet)
+
+    df = pd.DataFrame(tweet_list)
+    df.to_csv('s3://vibhi-airflow/elonmusk_twitter_data.csv', index=False)
