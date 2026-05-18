c2bb3180d297f2646c8c2c02656d368f037c5636
Vaibhav <vaibhavpanade57@gmail.com>
2026-05-18 15:29:54 +0530

    Update twitter_dag.py

--- twitter_dag.py
+++ twitter_dag.py
@@ -1,32 +1,51 @@
-from datetime import timedelta
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.utils.dates import days_ago
+import tweepy
+import pandas as pd
+import json
 from datetime import datetime
-from twitter_etl import run_twitter_etl
+import s3fs
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2020, 11, 8),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1)
-}
+def run_twitter_etl():
+    # Twitter API credentials
+    consumer_key = 'YFLFLCN2gGHPD0c3pn0kNCn9O'
+    consumer_secret = 'N83lAHwI4IVoFzRdiJRD4dFMo3FZRA9RYbpZbbhoJ2l6WMaK1R'
+    access_token = '2012575223879528449-0cmePJwOURv2Z2UdOr5M8uxBwCV0ZV'
+    access_token_secret = '8a4D2qNzzrWWHvNKFNlEeaKQ3S4BiTdepvKY6c0c6EQhp'
 
-dag = DAG(
-    'twitter_dag',
-    default_args=default_args,
-    description='Our first DAG with ETL process!',
-    schedule_interval=timedelta(days=1),
-)
+    # Twitter authentication
+    auth = tweepy.OAuth1UserHandler(
+        consumer_key,
+        consumer_secret
+    )
 
-run_etl = PythonOperator(
-    task_id='complete_twitter_etl',
-    python_callable=run_twitter_etl,
-    dag=dag, 
-)
+    auth.set_access_token(
+        access_token,
+        access_token_secret
+    )
 
-run_etl
+    api = tweepy.API(auth)
+
+    tweets = api.user_timeline(
+        screen_name='elonmusk',
+        include_rts=False,
+        count=200,
+        tweet_mode='extended'
+    )
+
+    tweet_list = []
+
+    for tweet in tweets:
+
+        text = tweet._json["full_text"]
+
+        refined_tweet = {
+            "user": tweet.user.screen_name,
+            "text": text,
+            "favorite_count": tweet.favorite_count,
+            "retweet_count": tweet.retweet_count,
+            "created_at": tweet.created_at
+        }
+
+        tweet_list.append(refined_tweet)
+
+    df = pd.DataFrame(tweet_list)
+    df.to_csv('s3://vibhi-airflow/elonmusk_twitter_data.csv', index=False)
